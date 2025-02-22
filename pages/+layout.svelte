e8b7d34f73012fbce261b2c5260ae7d2d3eaab80
Yngve Hoiseth <yngve@hoiseth.net>
2025-02-22 11:09:59 +0100

    GitHub repo

--- pages/+layout.svelte
+++ pages/+layout.svelte
@@ -5,6 +5,6 @@
 	export let data;
 </script>
 
-<EvidenceDefaultLayout {data} title="Dag 1">
+<EvidenceDefaultLayout {data} title="Dag 1" githubRepo="https://github.com/dag1-no/dag1">
 	<slot slot="content" />
 </EvidenceDefaultLayout>
