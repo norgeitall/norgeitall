4a2d12b19cb9bd291ce482f056de128f23da226f
Yngve Hoiseth <yngve@hoiseth.net>
2025-03-01 08:06:26 +0100

    Goatcounter

--- pages/+layout.svelte
+++ pages/+layout.svelte
@@ -3,8 +3,17 @@
 	import '../app.css';
 	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
 	export let data;
+	const env = import.meta.env;
 </script>
 
 <EvidenceDefaultLayout {data} title="Norge i tall" githubRepo="https://github.com/norgeitall/norgeitall">
 	<slot slot="content" />
 </EvidenceDefaultLayout>
+
+{#if env.PROD}
+	<script
+		data-goatcounter="https://norgeitall.goatcounter.com/count"
+		async
+		src="//gc.zgo.at/count.js"
+	></script>
+{/if}
